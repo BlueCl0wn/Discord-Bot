70b54f6b4bc29bd11e3f91939e965bbca82bb2a0
BlueCl0wn <45670653+BlueCl0wn@users.noreply.github.com>
2019-02-16 23:40:57 +0100

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+
+print(discord.__version__)
+print("----------------")
+print(" ")
+
+client = commands.Bot(command_prefix = ".")
+
+@client.event
+async def on_ready():
+    print("Looged in as")
+    print(client.user.name)
+    print(client.user.id)
+    print("----------------")
+
+@client.event
+async def on_message(message):
+    if message.content == "cookie":
+        await client.send_message(message.channel, ":cookie:")
+
+client.run("NTQzMTgzMjYzNjgyNzg5NDA3.D0jhVA.ryvcBQy0kK2zUdXEM-jhsUj1kgY")
