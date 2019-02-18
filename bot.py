eb18219719cea0116e8512af36689b84e1e48828
BlueCl0wn <45670653+BlueCl0wn@users.noreply.github.com>
2019-02-18 22:28:39 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,42 @@
 import discord
 from discord.ext import commands
+import time
 
 print(discord.__version__)
 print("----------------")
 
 client = commands.Bot(command_prefix = ".")
 
+
+#def emoji(smiley):
+    #if ":"+ smiley + ":" not in message.content:
+        #if smiley in message.content:
+            #string = message.content
+            #start = string.find(smiley)
+            #end = string.find(smiley) + 8
+            #text = string[:start] + ":" + smiley + ":" + string[end:]
+            #await client.send_message(message.channel, text)
+#emoji("eggplant") """In on_message() function."""
+
 @client.event
 async def on_ready():
-    print("Looged in as")
+    print("Logged in as")
     print(client.user.name)
     print(client.user.id)
     print("----------------")
 
 @client.event
 async def on_message(message):
-    if message.content == "cookie":
-        await client.send_message(message.channel, ":cookie:")
+    if message.author.id != client.user.id:
+        if message.content == "cookie":
+            await client.send_message(message.channel, ":cookie:")
+        if message.content == "hello":
+            await client.send_message(message.channel, "Hello %s" % (message.author.name))
+        if message.content.upper().startswith("/SAY"):
+            args = message.content.split(" ")
+            await client.send_message(message.channel, " ".join(args[1:]))
+            await client.delete_message(message)
+        if "DAREK" in message.content.upper():
+            await client.send_message(message.channel, "__**Darek macht nur Scheiße!**__")
 
 client.run("NTQzMTgzMjYzNjgyNzg5NDA3.D0jhVA.ryvcBQy0kK2zUdXEM-jhsUj1kgY")
